741e91dfffde493b5681eb10306d3d663395f12c
Calabronx <sebicala@hotmail.com>
2024-08-01 19:59:24 -0300

    pause state class to work with the input and pause of the game events

--- src/state_machine/states/PauseState.h
+++ src/state_machine/states/PauseState.h
@@ -1,5 +1,26 @@
 #pragma once
-class PauseState
+#ifndef PAUSE_STATE_H
+#define PAUSE_STATE_H
+
+#include "../State.h"
+
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/Text.hpp>
+
+class PauseState : public State
 {
+	public:
+			PauseState(StateStack& stack, Context context);
+
+			virtual void draw();
+			virtual bool update(sf::Time dt);
+			virtual bool handleEvent(const sf::Event& event);
+
+	private:
+		sf::Sprite	mBackgroundSprite;
+		sf::Text	mPausedText;
+		sf::Text	mInstructionText;
 };
+
+#endif
 
